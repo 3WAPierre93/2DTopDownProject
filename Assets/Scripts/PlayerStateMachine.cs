e9a78d8dc41456efa40f697feee084cca4487f68
3WAPierre93 <pierre.allileche@3wa.io>
2023-11-20 14:14:17 +0100

    Initial commit

---
+++ Assets/Scripts/PlayerStateMachine.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+public enum PlayerState
+{
+    IDLE,
+    RUN,
+    SPRINT,
+    ROLL
+}
+
+
+
+public class PlayerStateMachine : MonoBehaviour
+{
+    #region Main Methods
+
+    private void OnStateEnter()
+    {
+       
+
+    }
+
+    #endregion
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# 2DTopDownProject
+ In Progress
